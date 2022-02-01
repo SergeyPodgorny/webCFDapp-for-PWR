45354ca8e5c2fa9ab127cfbfce1835bb1a19a14b
SergeyPodgorny <serkonpod@gmail.com>
2022-02-01 12:15:18 +0300

    initial commit

---
+++ src/main/java/com/webCFD/webCFDapp/WebCfDappApplication.java
@@ -0,0 +1,13 @@
+package com.webCFD.webCFDapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCfDappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCfDappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webCFD/webCFDapp/WebCfDappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webCFD.webCFDapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCfDappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
