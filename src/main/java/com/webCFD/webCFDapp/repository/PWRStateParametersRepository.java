504537f6c08c26f95ba45c56458db17187b01570
SergeyPodgorny <serkonpod@gmail.com>
2022-04-07 11:24:25 +0300

    global refactoring

--- src/main/java/com/webCFD/webCFDapp/repository/PWRStateParametersRepository.java
+++ src/main/java/com/webCFD/webCFDapp/repository/PWRStateParametersRepository.java
@@ -1,9 +1,12 @@
 package com.webCFD.webCFDapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.webCFD.webCFDapp.entities.PWRStateParameters;
 
+
+@Repository
 public interface PWRStateParametersRepository extends JpaRepository<PWRStateParameters, Long> {
 
 	
